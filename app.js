fe2d0bef8cffbbb8d57b69a13182bb3f30229007
alison <alison@gmail.com>
2026-08-31 09:59:37 -0500

    Envio de informacion con JSON

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const express = requiere ('express');
+const express = require ('express');
 const app = express();
 const port = process.env.MIPUERTO || 3003; 
+//middle warc body_parse
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API Rest Full con express');
 });
 
+app.get('/api/aprendices', (req, res) => {
+res.status(200).json({mensaje: "Lista aprendices "})
+});
+
+app.post('/api/aprendices', (req, res) => {
+  const datosAprendiz= req.body
+  const edad = req.body.edad
+  if (edad < 18) {
+    return res.status(201).json({ mensaje: "Crear aprendices", Datos: datosAprendiz, Edad: "Eres menor de edad"})
+  } else {
+    return res.status(201).json({ mensaje: "Crear aprendices", Datos: datosAprendiz, Edad: "Eres mayor de edad"})
+}});
+
+app.put('/api/aprendices/:id', (req, res) => {
+res.status(200).json({mensaje: "Actualizar aprendiz "})
+});
+
+app.delete('/api/aprendices', (req, res) => {
+res.status(200).json({mensaje: "Eliminado "})
+});
+
 app.listen(port, () => {
-console.log( `Servidor en funcionamiento en el puerto:${port}`);
+ console.log( `Servidor en funcionamiento en el puerto: http://localhost:${port}`);
 });
